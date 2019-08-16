366f0d6ea587c293e1a39ae0aa2fdc26cd14a9fa
Paulljc <714978398@qq.com>
2019-08-16 22:34:35 +0800

    finish refactor and testValidateDate

--- src/main/java/sales/SalesApp.java
+++ src/main/java/sales/SalesApp.java
@@ -7,62 +7,90 @@ import java.util.List;
 
 public class SalesApp {
 
+    SalesDao salesDao;
+    SalesReportDao salesReportDao;
+    EcmService ecmService;
+
 	public void generateSalesActivityReport(String salesId, int maxRow, boolean isNatTrade, boolean isSupervisor) {
-		
-		SalesDao salesDao = new SalesDao();
-		SalesReportDao salesReportDao = new SalesReportDao();
-		List<String> headers = null;
-		
-		List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
-		
-		if (salesId == null) {
-			return;
-		}
-		
-		Sales sales = salesDao.getSalesBySalesId(salesId);
-		
-		Date today = new Date();
-		if (today.after(sales.getEffectiveTo())
-				|| today.before(sales.getEffectiveFrom())){
-			return;
-		}
-		
-		List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
-		
-		for (SalesReportData data : reportDataList) {
-			if ("SalesActivity".equalsIgnoreCase(data.getType())) {
-				if (data.isConfidential()) {
-					if (isSupervisor) {
-						filteredReportDataList.add(data);
-					}
-				}else {
-					filteredReportDataList.add(data);
-				}
-			}
-		}
-		
-		List<SalesReportData> tempList = new ArrayList<SalesReportData>();
-		for (int i=0; i < reportDataList.size() || i < maxRow; i++) {
-			tempList.add(reportDataList.get(i));
-		}
-		filteredReportDataList = tempList;
-		
+
+        List<String> headers = null;
+
+        if (salesId == null) {
+            return;
+        }
+
+        Sales sales = salesDao.getSalesBySalesId(salesId);
+
+        if (validateDate(sales)) {
+            return;
+        }
+
+        List<SalesReportData> reportDataList = salesReportDao.getReportData(sales);
+
+        List<SalesReportData> filteredReportDataList = new ArrayList<SalesReportData>();
+
+        filteredReportDataList = filterReport(reportDataList, isSupervisor, filteredReportDataList);
+
+        List<SalesReportData> tempList = generateTempList(maxRow, reportDataList);
+
+        filteredReportDataList = tempList;
+
+		headers = generateHeaders(isNatTrade);
+
+        SalesActivityReport report = this.generateReport(headers, reportDataList);
+
+		transferReportToXml(ecmService, report);
+
+    }
+
+	private void transferReportToXml(EcmService ecmService, SalesActivityReport report) {
+		ecmService.uploadDocument(report.toXml());
+	}
+
+	protected List<String> generateHeaders(boolean isNatTrade) {
+		List<String> headers;
 		if (isNatTrade) {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Time");
 		} else {
 			headers = Arrays.asList("Sales ID", "Sales Name", "Activity", "Local Time");
 		}
-		
-		SalesActivityReport report = this.generateReport(headers, reportDataList);
-		
-		EcmService ecmService = new EcmService();
-		ecmService.uploadDocument(report.toXml());
-		
+		return headers;
 	}
 
-	private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	protected List<SalesReportData> generateTempList(int maxRow, List<SalesReportData> reportDataList) {
+        List<SalesReportData> tempList = new ArrayList<>();
+        for (int i = 0; i < reportDataList.size() && i < maxRow; i++) {
+            tempList.add(reportDataList.get(i));
+        }
+        return tempList;
+    }
+
+    protected List<SalesReportData> filterReport(List<SalesReportData> reportDataList, boolean isSupervisor, List<SalesReportData> filteredReportDataList) {
+        for (SalesReportData data : reportDataList) {
+            if ("SalesActivity".equalsIgnoreCase(data.getType())) {
+                if (data.isConfidential()) {
+                    if (isSupervisor) {
+                        filteredReportDataList.add(data);
+                    }
+                } else {
+                    filteredReportDataList.add(data);
+                }
+            }
+        }
+        return filteredReportDataList;
+    }
+
+    protected boolean validateDate(Sales sales) {
+        Date today = new Date();
+        if (today.after(sales.getEffectiveTo()) || today.before(sales.getEffectiveFrom())) {
+            return true;
+        }
+        return false;
+    }
+
+    private SalesActivityReport generateReport(List<String> headers, List<SalesReportData> reportDataList) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -1,14 +1,41 @@
 package sales;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SalesAppTest {
+    @Mock
+    SalesDao salesDao;
+    @Mock
+    SalesReportDao salesReportDao;
+    @Mock
+    EcmService ecmService;
 
-	@Test
-	public void testGenerateReport() {
-		
-		SalesApp salesApp = new SalesApp();
-		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
-	}
+    @Test
+    public void testValidateDate() {
+        //given
+        SalesApp salesApp = new SalesApp();
+        Sales sales = spy(Sales.class);
+        doReturn(new Date(new Date().getTime() + 60 * 60 * 1000)).when(sales).getEffectiveTo();
+        doReturn(new Date(new Date().getTime() - 60 * 60 * 1000)).when(sales).getEffectiveFrom();
+        //when
+        boolean isValidDate = salesApp.validateDate(sales);
+        //then
+        Assert.assertFalse(isValidDate);
+    }
+
 }
