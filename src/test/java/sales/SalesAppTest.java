e4f88d5cdf389dc8ed2b02855eee2ceb524e9947
Paulljc <714978398@qq.com>
2019-08-16 22:44:51 +0800

    test filterReportDataList

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -13,8 +13,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SalesAppTest {
@@ -24,6 +24,8 @@ public class SalesAppTest {
     SalesReportDao salesReportDao;
     @Mock
     EcmService ecmService;
+    @InjectMocks
+    SalesApp salesApp = new SalesApp();
 
     @Test
     public void testValidateDate() {
@@ -32,10 +34,31 @@ public class SalesAppTest {
         Sales sales = spy(Sales.class);
         doReturn(new Date(new Date().getTime() + 60 * 60 * 1000)).when(sales).getEffectiveTo();
         doReturn(new Date(new Date().getTime() - 60 * 60 * 1000)).when(sales).getEffectiveFrom();
+
         //when
         boolean isValidDate = salesApp.validateDate(sales);
+
         //then
         Assert.assertFalse(isValidDate);
     }
 
+    @Test
+    public void testFilterList_givenReportDataAndIsSupervisor_thenGetFilteredReportDataList() {
+        //given
+        Sales sales = new Sales();
+        SalesReportData salesReportData = mock(SalesReportData.class);
+        when(salesReportData.getType()).thenReturn("SalesActivity");
+        List<SalesReportData> reportDataList = Arrays.asList(new SalesReportData(), salesReportData);
+        when(salesReportDao.getReportData(sales)).thenReturn(reportDataList);
+
+        //when
+        List<SalesReportData> filteredReportDataList = new ArrayList<>();
+        filteredReportDataList = salesApp.filterReport(reportDataList, true, filteredReportDataList);
+
+        //then
+        Assert.assertEquals(1, filteredReportDataList.size());
+        Assert.assertEquals("SalesActivity", filteredReportDataList.get(0).getType());
+    }
+
+    
 }
